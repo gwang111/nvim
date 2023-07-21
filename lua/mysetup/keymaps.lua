fa68207a3e74d0c1763e529fde16c74ac03133b0
Gary Wang <garywan@amazon.com>
2023-07-21 02:22:25 +0000

    update keymaps

--- lua/mysetup/keymaps.lua
+++ lua/mysetup/keymaps.lua
@@ -3,11 +3,11 @@ local map = vim.keymap.set
 vim.g.mapleader = ','
 
 -- buffer mappings
-map("n", "[b", ":bprevious<CR>", { desc = "Previous buffer" })
-map("n", "]b", ":bnext<CR>", { desc = "Next buffer" })
-map("n", "[B", ":bfirst<CR>", { desc = "First buffer" })
-map("n", "]B", ":blast<CR>", { desc = "Last buffer" })
-map("n", "]q", ":bd<CR>", { desc = "Close buffer" })
+map("n", "[p", ":bprevious<CR>", { desc = "Previous buffer" })
+map("n", "]p", ":bnext<CR>", { desc = "Next buffer" })
+map("n", "[P", ":bfirst<CR>", { desc = "First buffer" })
+map("n", "]P", ":blast<CR>", { desc = "Last buffer" })
+map("n", "[o", ":bd<CR>", { desc = "Close buffer" })
 
 -- telescope mappings
 local builtin = require('telescope.builtin')
